41d0fe3198f10248bdb18f5436d0b05c4822599e
adishimo <77383584+adishimo@users.noreply.github.com>
2021-01-23 17:26:06 +0200

    added table

--- tasks.js
+++ tasks.js
@@ -71,3 +71,45 @@ let task9 = {
   tasksFinished: 2,
   topic: "js",
 };
+let taskTable = [task1, task2, task3, task4, task5, task6, task7, task8, task9];
+// add total time of work and finished percent  properties to each work
+for (let task of taskTable) {
+  task["totalTime"] = (task.finishedAt - task.startedAt) / 3600000;
+  task["tasksFinishedPercent"] = Math.floor(
+    (task.tasksFinished / task.tasksGiven) * 100
+  );
+  task.startedAt = task.startedAt.toLocaleTimeString();
+  task.finishedAt = task.finishedAt.toLocaleTimeString();
+}
+console.log(task1.startedAt);
+
+let tableHead = [
+  "started At",
+  "finished At",
+  "tasks Given",
+  "tasks Finished",
+  "topic",
+  "totalTime",
+  "tasksFinishedPercent",
+];
+document.write("<table>");
+document.write("<tr>");
+for (let head of tableHead) {
+  document.write("<th>" + head + "</th>");
+}
+
+document.write("</tr>");
+for (let task of taskTable) {
+  document.write("<tr>");
+  document.write("<td>" + task.startedAt + "</td>");
+  document.write("<td>" + task.finishedAt + "</td>");
+  document.write("<td>" + task.tasksGiven + "</td>");
+  document.write("<td>" + task.tasksFinished + "</td>");
+  document.write("<td>" + task.topic + "</td>");
+  document.write("<td class = 'totalTime'>" + task.totalTime + "</td>");
+  document.write(
+    "<td class = 'percent'>" + task.tasksFinishedPercent + "</td>"
+  );
+  document.write("</tr>");
+}
+document.write("</table>");
