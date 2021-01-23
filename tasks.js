e4b9a1e8afa7502cdf7746056373c8aecdfc487f
adishimo <77383584+adishimo@users.noreply.github.com>
2021-01-23 17:25:26 +0200

    added objects

---
+++ tasks.js
@@ -0,0 +1,73 @@
+"use strict";
+
+let task1 = {
+  startedAt: new Date("2021-01-3:13:00"),
+  finishedAt: new Date("2021-01-11:19:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "Html",
+};
+
+let task2 = {
+  startedAt: new Date("2021-01-4:13:00"),
+  finishedAt: new Date("2021-01-4:22:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "Html",
+};
+
+let task3 = {
+  startedAt: new Date("2021-01-5:13:00"),
+  finishedAt: new Date("2021-01-5:23:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "Html",
+};
+
+let task4 = {
+  startedAt: new Date("2021-01-6:13:00"),
+  finishedAt: new Date("2021-01-6:19:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "Html",
+};
+
+let task5 = {
+  startedAt: new Date("2021-01-7:14:00"),
+  finishedAt: new Date("2021-01-7:00:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "css",
+};
+
+let task6 = {
+  startedAt: new Date("2021-01-8:11:00"),
+  finishedAt: new Date("2021-01-8:22:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "css",
+};
+
+let task7 = {
+  startedAt: new Date("2021-01-9:12:00"),
+  finishedAt: new Date("2021-01-9:23:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "js",
+};
+
+let task8 = {
+  startedAt: new Date("2021-01-10:11:00"),
+  finishedAt: new Date("2021-01-10:22:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "js",
+};
+
+let task9 = {
+  startedAt: new Date("2021-01-11:10:00"),
+  finishedAt: new Date("2021-01-11:23:00"),
+  tasksGiven: 2,
+  tasksFinished: 2,
+  topic: "js",
+};
